c705cd3a867c64524a65e235ebf9f7f7e3f3ff52
Tim Angevare <t.angevare.uu.nl>
2021-03-28 10:44:01 +0200

    added project

---
+++ ui.py
@@ -0,0 +1,82 @@
+import tkinter as tk
+from tkinter import Label, font
+from tkinter.constants import ANCHOR
+from typing import Text
+import webscraper
+from PIL import Image, ImageTk
+from time import sleep
+from random import randint
+
+HEIGHT = 700
+WIDTH = 1024
+yellow = "#1a0000"
+##000000
+gray = "#e5e5e5"
+
+root = tk.Tk()
+root.title("GUI Assistant")
+
+def wrap_by_word(s, n):
+    a = s.split()
+    ret = ''
+    for i in range(0, len(a), n):
+        ret += ' '.join(a[i:i+n]) + '\n'
+
+    return ret
+
+def quotes():
+    file = open("quotes.txt", "r")
+    quotes = file.readlines()
+    line = quotes[randint(0, len(quotes) - 1)]
+    return wrap_by_word(line, 25)
+    
+
+def GUI():
+    try:
+        webscraper.quit()
+        sleep(1)
+    except:
+        pass
+    webscraper.start()
+    global image
+    i = Image.open("./galaxy.jpg")
+    i.thumbnail((2000, 1000))
+
+    image = ImageTk.PhotoImage( i , master=root)
+    my_label = Label(root, image=image )
+    my_label.place(x=0, y=0, relheight=1,relwidth=1)    
+
+    #canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
+    #canvas.pack()
+
+    top = tk.Frame(root, bg=yellow)
+    top.place(relheight=0.2, relwidth=0.95, rely=0.025, relx=0.025)
+
+    quote = tk.Label(top,  fg=gray, text=quotes(), bg=yellow, font=("Pacifico", 25))
+    quote.place(relheight=0.8, relwidth=0.9, relx=0.05, rely=0.1)
+
+    temp_holder = tk.Frame(root, bg=yellow)
+    temp_holder.place(relheight=0.25, relwidth=0.2, rely=0.275, relx=0.025)
+
+    temp = tk.Label(temp_holder, fg=gray, text="Degrees room \n Degrees outside: " + str(webscraper.weather()), font=("Impact", 20), bg=yellow)
+    temp.place(relheight=0.8, relwidth=0.9, relx=0.05, rely=0.1)
+
+    icloud_holder = tk.Frame(root, bg=yellow)
+    icloud_holder.place(relheight=0.40, relwidth=0.33, rely=0.275, relx=0.25)
+    calender = tk.Label(icloud_holder, text=webscraper.icloud(),fg=gray,  font=("Impact", 20), bg=yellow)
+    calender.place(relheight=0.99, relwidth=0.99, relx=0.005, rely=0.005)
+
+    news_holder = tk.Frame(root, bg=yellow)
+    news_holder.place(relheight=0.575, relwidth=0.25, relx=0.605 , rely=0.275)
+    nieuws = webscraper.news()
+    verhalen = []
+    for x in range(len(nieuws)):
+        verhalen.append(wrap_by_word(nieuws[x], 4))
+    nieuws = " \n ".join(verhalen)
+
+    news = tk.Label(news_holder, fg=gray, bg=yellow, font=('impact',18), text=nieuws)
+    news.place(relheight=0.95, relwidth=0.9, relx=0.05, rely=0.025)
+
+root.after(70000, GUI())
+root.attributes("-fullscreen", True)
+root.mainloop()
---
+++ webscraper.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from icalevents.icalevents import events
+#pip3 install icalevents
+
+ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,)
+def start():
+    global driver
+    driver = webdriver.Chrome(executable_path="/Users/timangevare/Documents/Code/chromedriver")
+    global wait
+    wait = WebDriverWait(driver, 60, ignored_exceptions=ignored_exceptions)
+
+def weather():
+    driver.get("https://weather.com/weather/today/l/57fae565d25f68b70131cd8683da28e8b595fb41ae07ae5cd2a827af58233bac")
+    sleep(4)
+    temp = driver.find_element_by_class_name("CurrentConditions--tempValue--3KcTQ")
+    farenheit = int(temp.text[0:-1])
+    celsuis = int((farenheit - 32)/ (9/5))
+    return celsuis
+
+def news():
+    global links
+    links = []
+    news = []
+    driver.get("https://www.forbes.com/?sh=43e5cbfe2254")
+    agree = wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "trustarc-agree-btn")))
+    agree.click()
+    stories = wait.until(expected_conditions.presence_of_all_elements_located((By.CLASS_NAME,"body--dense-merriweather")))
+    for i in range(3):
+        #link = stories[i].get_attribute('href')
+        news.append(stories[i].text)
+        #links.append(link)
+    return(news)
+
+def icloud():
+    sentence = ""
+    es = events(url="webcal://p65-caldav.icloud.com/published/2/MTc1NTUwNDcyMzE3NTU1MIlWSz4EvoYPW425i3rzk4W1cefZ14LIwtcNYllAipAB", fix_apple=True)
+    for i in range(3):
+        sentence = sentence + "\n" + str(es[i])
+    return sentence
+
+
+
+def quit():
+    driver.quit()
